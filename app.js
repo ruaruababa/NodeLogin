b14be8f18038965cbe7af7e97354a478c5d22a5d
ruaruababa <dethia1toanlyanh@gmail.com>
2022-02-08 07:47:16 +0700

    upload

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//Include Packages and routes
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -13,15 +12,25 @@ var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var connection = require('./lib/db');
 
-
-var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
 
 var app = express();
 
 // view engine setup
+
+//Make sure your Express App has this setup,
+//for the current layout it sounds like you have.
+
+// Require static assets from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set 'views' directory for any views 
+// being rendered res.render()
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+
+// Set view engine as EJS
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -29,9 +38,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Setting session
 app.use(session({
-    secret: '1900100co',
+    secret: '123456cat',
     resave: false,
     saveUninitialized: true,
     cookie: { maxAge: 60000 }
@@ -40,7 +48,6 @@ app.use(session({
 app.use(flash());
 app.use(expressValidator());
 
-app.use('/', indexRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
@@ -58,5 +65,8 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
-
+// port must be set to 3000 because incoming http requests are routed from port 80 to port 8080
+app.listen(3000, function() {
+    console.log('Node app is running on port 3000');
+});
 module.exports = app;
